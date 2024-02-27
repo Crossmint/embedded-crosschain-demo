a00aadcc4b1e7bce246a99827b975e9bb5f2a75f
Danny Mulvihill <danny@paella.dev>
2024-02-26 20:32:19 -0800

    wip dynamic

--- app/components/Crossmint.tsx
+++ app/components/Crossmint.tsx
@@ -1,25 +1,98 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { CrossmintPaymentElement } from "@crossmint/client-sdk-react-ui";
-import { ethers } from "ethers";
+//import { DynamicWidget, useDynamicContext } from "@dynamic-labs/sdk-react-core";
+import { DynamicWidget, useDynamicContext } from "@dynamic-labs/sdk-react";
 import Minting from "./Minting";
 
-type CrossmintProps = {
-  signer: ethers.providers.JsonRpcSigner;
-  accounts: string[];
-};
+type Chain =
+  | "base"
+  | "polygon"
+  | "solana"
+  | "cardano"
+  | "sui"
+  | "ethereum"
+  | "ethereum-sepolia"
+  | "bsc"
+  | "optimism"
+  | "arbitrum"
+  | "zora"
+  | "arbitrumnova"
+  | "zkatana";
 
-const Crossmint: React.FC<CrossmintProps> = ({ signer, accounts }) => {
+const Crossmint: React.FC = () => {
   const [orderIdentifier, setOrderIdentifier] = useState<string | null>(null);
+  const [signer, setSigner] = useState<any>();
+  const [address, setAddress] = useState("");
+  const [network, setNetwork] = useState<Chain>("ethereum");
+  const { walletConnector } = useDynamicContext();
 
   const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as string;
   const collectionId = process.env.NEXT_PUBLIC_COLLECTION_ID as string;
   const environment = process.env.NEXT_PUBLIC_ENVIRONMENT as string;
 
+  const getChainId = (chain: string) => {
+    const chains = {
+      arbitrum: 421613, // goerli
+      //arbitrum: 421614, // sepolia
+      //arbitrum: 42161, // mainnet
+      base: 84531, // goerli
+      //base: 84532, // sepolia
+      //base: 8453, // mainnet
+      ethereum: 5, //
+      //ethereum: 11155111, // sepolia
+      //ethereum: 1, // mainnet
+      optimism: 420, // goerli
+      //optimism: 11155420, // sepolia
+      //optimism: 10, // mainnet
+    };
+
+    switch (chain) {
+      case "arbitrum":
+        return chains.arbitrum;
+      case "ethereum":
+        return chains.ethereum;
+      case "base":
+        return chains.base;
+      case "optimism":
+        return chains.optimism;
+    }
+  };
+
+  useEffect(() => {
+    async function getWalletSigner() {
+      try {
+        const _signer = await walletConnector?.getSigner();
+
+        console.log(_signer);
+        const _address = await walletConnector?.fetchPublicAddress();
+        setSigner(_signer);
+        setAddress(_address || "");
+      } catch (error) {
+        console.error("Error getting Ethereum signer:", error);
+      }
+    }
+
+    getWalletSigner();
+  }, [walletConnector]);
+
+  if (
+    signer == null ||
+    !address ||
+    !["EVM", "ETH"].includes(walletConnector?.connectedChain || "")
+  ) {
+    return (
+      <p className="mt-2 text-s">
+        Connect your ETH(Testnet) wallet to proceed.
+      </p>
+    );
+  }
+
   return (
     <>
       <div className="sm:col-span-3">
+        <DynamicWidget />
         {orderIdentifier === null ? (
           <CrossmintPaymentElement
             projectId={projectId}
@@ -27,15 +100,19 @@ const Crossmint: React.FC<CrossmintProps> = ({ signer, accounts }) => {
             environment={environment}
             paymentMethod="ETH"
             signer={{
-              address: accounts[0],
+              address: address,
               signAndSendTransaction: async (transaction) => {
-                const response = await signer.sendTransaction({
-                  ...transaction,
-                  type: transaction.type!,
-                });
-
-                return response.hash;
+                const signRes = await signer.sendTransaction(transaction);
+                return signRes.hash;
               },
+              handleChainSwitch: async (chain) => {
+                await walletConnector!.switchNetwork({
+                  networkChainId: getChainId(chain),
+                });
+                setNetwork(chain);
+              },
+              supportedChains: ["arbitrum", "ethereum", "optimism", "base"],
+              chain: network,
             }}
             mintConfig={{
               type: "erc-721",
--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,152 @@
 import React from "react";
 import CollectionInfo from "./components/CollectionInfo";
 import Crossmint from "./components/Crossmint";
-import useEthersSigner from "./hooks/useEthersSigner";
+// import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+// import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum-all";
+
+export const DYNAMIC_EVM_WALLET_CONNECTOR_NETWORKS = [
+  {
+    blockExplorerUrls: ["https://etherscan.io/"],
+    chainId: 1,
+    chainName: "Ethereum Mainnet",
+    iconUrls: ["https://app.dynamic.xyz/assets/networks/eth.svg"],
+    name: "Ethereum",
+    nativeCurrency: {
+      decimals: 18,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    networkId: 1,
+
+    rpcUrls: ["https://mainnet.infura.io/v3/"],
+    vanityName: "ETH Mainnet",
+  },
+  {
+    blockExplorerUrls: ["https://etherscan.io/"],
+    chainId: 5,
+    chainName: "Ethereum Goerli",
+    iconUrls: ["https://app.dynamic.xyz/assets/networks/eth.svg"],
+    name: "Ethereum",
+    nativeCurrency: {
+      decimals: 18,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    networkId: 5,
+    rpcUrls: ["https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"],
+
+    vanityName: "Goerli",
+  },
+  {
+    blockExplorerUrls: ["https://optimistic.etherscan.io/"],
+    chainId: 10,
+    chainName: "OP Mainnet",
+    iconUrls: ["https://app.dynamic.xyz/assets/networks/optimism.svg"],
+    name: "OP Mainnet",
+    nativeCurrency: {
+      decimals: 18,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    networkId: 10,
+    rpcUrls: ["https://mainnet.optimism.io/"],
+    vanityName: "OP",
+  },
+  {
+    blockExplorerUrls: ["https://basescan.org"],
+    chainId: 8453,
+    chainName: "Base",
+    iconUrls: ["https://app.dynamic.xyz/assets/networks/base.svg"],
+    name: "Base",
+    nativeCurrency: {
+      decimals: 18,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    networkId: 8453,
+    rpcUrls: ["https://mainnet.base.org"],
+    vanityName: "Base",
+  },
+  {
+    blockExplorerUrls: ["https://explorer.arbitrum.io"],
+    chainId: 42161,
+    chainName: "Arbitrum One",
+    iconUrls: ["https://app.dynamic.xyz/assets/networks/arbitrum.svg"],
+    name: "Arbitrum One",
+    nativeCurrency: {
+      decimals: 18,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    networkId: 42161,
+    rpcUrls: ["https://goerli-rollup.arbitrum.io/rpc"],
+    vanityName: "Arbitrum One",
+  },
+  {
+    blockExplorerUrls: ["https://goerli.arbiscan.io"],
+    chainId: 421613,
+    chainName: "Arbitrum Goerli",
+    iconUrls: ["https://app.dynamic.xyz/assets/networks/arbitrum.svg"],
+    name: "Arbitrum Goerli",
+    nativeCurrency: {
+      decimals: 18,
+      name: "AGOR",
+      symbol: "AGOR",
+    },
+    networkId: 421613,
+    rpcUrls: ["https://goerli-rollup.arbitrum.io/rpc"],
+    vanityName: "Arbitrum Goerli",
+  },
+  {
+    blockExplorerUrls: ["https://nova-explorer.arbitrum.io"],
+    chainId: 42170,
+    chainName: "Arbitrum Nova",
+    iconUrls: ["https://app.dynamic.xyz/assets/networks/arbitrumnova.svg"],
+    name: "Arbitrum Nova",
+    nativeCurrency: {
+      decimals: 18,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    networkId: 42170,
+    rpcUrls: ["https://nova.arbitrum.io/rpc"],
+    vanityName: "Arbitrum Nova",
+  },
+  {
+    blockExplorerUrls: ["https://explorer.zora.energy"],
+    chainId: 7777777,
+    chainName: "Zora",
+    iconUrls: ["https://app.dynamic.xyz/assets/networks/zora.svg"],
+    name: "Zora",
+    nativeCurrency: {
+      decimals: 18,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    networkId: 7777777,
+    rpcUrls: ["https://rpc.zora.energy/"],
+    vanityName: "Zora",
+  },
+];
 
 const Page: React.FC = () => {
-  const { signer, accounts } = useEthersSigner();
-
   return (
-    <div className="container mx-auto max-w-4xl bg-white">
-      <div className="grid grid-cols-1 sm:grid-cols-5 sm:gap-4 p-4">
-        <CollectionInfo />
-        {signer && <Crossmint signer={signer} accounts={accounts} />}
+    <DynamicContextProvider
+      settings={{
+        environmentId: "73039506-8577-45f7-a8c6-3782b4e9be00",
+        walletConnectors: [EthereumWalletConnectors],
+        evmNetworks: DYNAMIC_EVM_WALLET_CONNECTOR_NETWORKS,
+      }}
+    >
+      <div className="container mx-auto max-w-4xl bg-white">
+        <div className="grid grid-cols-1 sm:grid-cols-5 sm:gap-4 p-4">
+          <CollectionInfo />
+          <Crossmint />
+        </div>
       </div>
-    </div>
+    </DynamicContextProvider>
   );
 };
 
